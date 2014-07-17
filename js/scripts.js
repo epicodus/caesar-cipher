8989b1f37b53c3589c75533cb8e0ffbb40b40d87
Eric <erjohnson.pdx@gmail.com>
2014-07-16 18:35:08 -0700

    :octopus: Add multi-word encoding functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,17 @@
 
 function makeSecret (message) {
 	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-	var messageArray = message.toLowerCase().split("");
+	var messageArray = message.toLowerCase().replace(/\s+/g, '-').split("");
 	var encodedMessageArray = [];
 
 	for (var i = 0; i < messageArray.length; i++) {
+		if (messageArray[i] === "-"){
+			encodedMessageArray.push(" ");
+		} else {
 		var x = alphabet.indexOf(messageArray[i]);
 		var y = alphabet[x + 3];
 		encodedMessageArray.push(y);
+		}
 	};
 
 	return encodedMessageArray.join("");
--- js/specs.js
+++ js/specs.js
@@ -7,4 +7,14 @@ describe('makeSecret', function() {
 			makeSecret("epicodus").should.equal("hslfrgxv");
 		});
 	});
+	describe('Encode a two-word message', function() {
+		it("returns 'hslfrgxv ' for the message 'epicodus rocks'.", function() {
+			makeSecret("epicodus rocks").should.equal("hslfrgxv urfnv");
+		});
+	});
+	describe('Encode a multi-word message', function() {
+		it("returns 'wkh phhwlqj lv wrpruurz' for the message 'the meeting is tomorrow'.", function() {
+			makeSecret("the meeting is tomorrow").should.equal("wkh phhwlqj lv wrpruurz");
+		});
+	});
 });
