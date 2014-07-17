92db5a5cdfd45c24c06ef105f8d5b9df1f55bdaa
Eric <erjohnson.pdx@gmail.com>
2014-07-16 18:02:34 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-// Project Scripts
+/* Caesar Cipher */
+
+function makeSecret (message) {
+	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	var messageArray = message.toLowerCase().split("");
+	var encodedMessageArray = [];
+
+	for (var i = 0; i < messageArray.length; i++) {
+		var x = alphabet.indexOf(messageArray[i]);
+		var y = alphabet[x + 3];
+		encodedMessageArray.push(y);
+	};
+
+	return encodedMessageArray.join("");
+};
--- js/specs.js
+++ js/specs.js
@@ -1,1 +1,10 @@
-// Tests Go Here!
+describe('makeSecret', function() {
+	describe('Encode a one-word message', function() {
+		it("returns 'fdw' for the message 'cat'.", function() {
+			makeSecret("cat").should.equal("fdw");
+		});
+		it("returns 'hslfrgxv' for the message 'epicodus'.", function() {
+			makeSecret("epicodus").should.equal("hslfrgxv");
+		});
+	});
+});
